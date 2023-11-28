49a2ee8d2f12148e762e1978f205a1e83bf8e45c
Nathan Thomas <nwthomas@me.com>
2023-11-27 19:59:38 -0800

    Finish building fib worker redis client

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,23 @@
 const keys = require("./keys");
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  redis_strategy: () => 1000,
+});
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) {
+    return 1;
+  }
+
+  return fib(index - 1) + index(index - 2);
+}
+
+sub.on("message", (_, message) => {
+  redisClient.hset("values", message, fib(parseInt(message, 10)));
+});
+
+sub.subscribe("insert");
