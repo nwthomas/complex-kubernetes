090c71b6c9aeb6d7de60b578a14f1412c5d266b8
Nathan Thomas <nwthomas@me.com>
2024-03-17 11:59:11 -0700

    Add greater fib index values

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,5 @@
-const keys = require('./keys');
-const redis = require('redis');
+const keys = require("./keys");
+const redis = require("redis");
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
@@ -9,11 +9,26 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
-  if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+  let first = 1;
+  let second = 1;
+
+  if (index <= 1) {
+    return 1;
+  }
+
+  let currentIndex = 1;
+
+  while (currentIndex < index) {
+    let temp = first + second;
+    first = second;
+    second = temp;
+    currentIndex += 1;
+  }
+
+  return second;
 }
 
-sub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fib(parseInt(message)));
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib(parseInt(message)));
 });
-sub.subscribe('insert');
+sub.subscribe("insert");
